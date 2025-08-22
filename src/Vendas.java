e205045d91fde277a3b89c9ef8c2dc0a24bd0ea8
Caroline Santana <css.carolinessantana@gmail.com>
2025-08-21 21:37:23 -0300

    Delete src directory

--- src/Vendas.java
+++
@@ -1,8 +0,0 @@
-/* Nome: Caroline Dos Santos Santana
-Pergunta:Você foi contratado para desenvolver um sistema de vendas para uma pequena loja. A loja vende produtos e cada produto tem um preço. Quando um cliente compra um produto, ele pode optar por pagar à vista ou parcelado. Caso opte por parcelar, será necessário calcular o valor das parcelas e se haverá juros. Quantas classes você enxerga sendo necessárias para resolver esse problema e o que cada uma delas faria? Descreva brevemente cada classe e seu papel no sistema.
-
-Resposta: Classe Venda, com atributos como cliente, produtos, forma de pagamento.
-Classe Produto, com atributos como nome e preço.
-Classe Cliente, com nome, CPF, histórico de compras.
-Classe Pagamento, com valor, tipo de compra (a vista/parcelado), numero de parcelas (para caso for parcelado)
-*  */
